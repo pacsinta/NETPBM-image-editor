e9ebc1d61487bd5416f7b09dc2a0a23c3a018cf8
pacsinta <61376206+pacsinta@users.noreply.github.com>
2020-11-29 23:24:10 +0100

    Add files via upload
    1.0 upload part 2

---
+++ cmake-build-debug/CMakeFiles/kepszerkesztes.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/image.c" "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/cmake-build-debug/CMakeFiles/kepszerkesztes.dir/image.c.obj"
+  "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/imageEditing.c" "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/cmake-build-debug/CMakeFiles/kepszerkesztes.dir/imageEditing.c.obj"
+  "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/imageReading.c" "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/cmake-build-debug/CMakeFiles/kepszerkesztes.dir/imageReading.c.obj"
+  "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/main.c" "C:/Users/csipa/Documents/Projects/School/Prog1/NHz1/kepszerkesztes/cmake-build-debug/CMakeFiles/kepszerkesztes.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/kepszerkesztes.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kepszerkesztes.dir/image.c.obj"
+  "CMakeFiles/kepszerkesztes.dir/imageEditing.c.obj"
+  "CMakeFiles/kepszerkesztes.dir/imageReading.c.obj"
+  "CMakeFiles/kepszerkesztes.dir/main.c.obj"
+  "kepszerkesztes.exe"
+  "kepszerkesztes.exe.manifest"
+  "kepszerkesztes.lib"
+  "kepszerkesztes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kepszerkesztes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
